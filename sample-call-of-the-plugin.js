59e07739a6d330498aa26e77d3d962ed9af6597b
tariqs-repo <tariq.information@gmail.com>
2015-04-16 02:10:02 +0600

    Update sample-call-of-the-plugin.js

--- sample-call-of-the-plugin.js
+++ sample-call-of-the-plugin.js
@@ -1,9 +1,13 @@
-realtimeNotification({
-	domSelector: '.record-container table tbody tr',
-	notificationCounterSelector: '#countNotification',
-	showNotificationOnCounterClick: true,
+notificationStateManager({
+	recordContentSelector: '.record-container table tbody tr',
+	counterSelector: '#countNotification',
+	showNotificationsOnCounterClick: true,
 	getListMethodName: 'notificationList', /* signalr server method to get notification list at session start */
-	getRecordMethodName: 'getNotified', /* client Method Name To Call From Server */
+	getRecordMethodName: 'getNotified', /* client Method Name To Call From Server when a new notification comes */
 	dateTimeFieldName: 'time',
-	signOutButtonSelector: 'input[value="Sign Out"]'
+	signOutButtonSelector: 'input[value="Sign Out"]',
+	max: 10, // 15 default
+	/* events */
+	onGetList: null,
+	onRecordArrival: null
 });
