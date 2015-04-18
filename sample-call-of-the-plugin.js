816d4699c803d38d26b573682d87ea8f55d8821d
tariqs-repo <tariq.information@gmail.com>
2015-04-18 06:53:32 +0600

    Update sample-call-of-the-plugin.js

--- sample-call-of-the-plugin.js
+++ sample-call-of-the-plugin.js
@@ -1,13 +1,13 @@
 notificationStateManager({
-	recordContentSelector: '.record-container table tbody tr',
-	counterSelector: '#countNotification',
-	showNotificationsOnCounterClick: true,
-	getListMethodName: 'notificationList', /* signalr server method to get notification list at session start */
-	getRecordMethodName: 'getNotified', /* client Method Name To Call From Server when a new notification comes */
-	dateTimeFieldName: 'time',
-	signOutButtonSelector: 'input[value="Sign Out"]',
-	max: 10, // 15 default
-	/* events */
-	onGetList: null,
-	onRecordArrival: null
-});
+			recordContentSelector: '.record-container table tbody tr',
+			counterSelector: '#countNotification',
+			getListMethodName: 'notificationList', /* signalr server method to get notification list at session start; list must be json array (or stringify json array) and in ascending order of ID (or CreationDate) */
+			getRecordMethodName: 'getNotified', /* client Method Name To Call From Server when a new notification comes; */
+			dateTimeFieldName: 'time', /* UTC date and JS valid format */
+			signOutButtonSelector: 'input[value="Sign Out"]',
+			/* optinoal */
+			showNotificationsOnCounterClick: true,
+			notificationPanelSelector: '.record-container',
+			onGetList: function(list) { console.log(list.length) },
+			onRecordArrival: function(obj) { console.log(obj.length) }
+		})
