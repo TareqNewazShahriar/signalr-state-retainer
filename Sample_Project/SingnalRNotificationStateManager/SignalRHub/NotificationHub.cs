9a79ea28061a3b469039f7067a4c3f76797c5717
tariqs-repo <tariq.information@gmail.com>
2015-04-29 00:15:30 +0600

    testing code removed of IE10/11 signalr

--- Sample_Project/SingnalRNotificationStateManager/SignalRHub/NotificationHub.cs
+++ Sample_Project/SingnalRNotificationStateManager/SignalRHub/NotificationHub.cs
@@ -23,8 +23,7 @@ namespace SingnalRNotificationStateManager.SignalRHub
 
 		public string notificationList()
 		{
-			//string jsonStr = new JavaScriptSerializer().Serialize(LogList.Logs.OrderByDescending(x => x.Id).Take(5));
-			return "[{\"Id\":5,\"Summary\":\"Something happened 5\",\"User\":\"user5\",\"CreationDate\":\"\\/Date(1430222916489)\\/\"},{\"Id\":4,\"Summary\":\"Something happened 4\",\"User\":\"user4\",\"CreationDate\":\"\\/Date(1430204916489)\\/\"},{\"Id\":3,\"Summary\":\"Something happened 3\",\"User\":\"user3\",\"CreationDate\":\"\\/Date(1430186916489)\\/\"},{\"Id\":2,\"Summary\":\"Something happened 2\",\"User\":\"user2\",\"CreationDate\":\"\\/Date(1430168916489)\\/\"},{\"Id\":1,\"Summary\":\"Something happened 1\",\"User\":\"user1\",\"CreationDate\":\"\\/Date(1430150916489)\\/\"}]";
+			return new JavaScriptSerializer().Serialize(LogList.Logs.OrderByDescending(x => x.Id).Take(5));
 		}
 	}
 }
